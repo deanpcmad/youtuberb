337b44b0cbe5d18f35471368e7e57aac3d4165aa
Dean Perry <dean@deanpcmad.com>
2021-12-30 13:31:49 +0000

    video part and basic options for title, description

--- lib/you_tube/objects/video.rb
+++ lib/you_tube/objects/video.rb
@@ -1,4 +1,19 @@
 module YouTube
   class Video < Object
+
+    def initialize(options = {})
+      super options
+
+      if options["snippet"]
+        self.title        = options["snippet"]["title"]
+        self.description  = options["snippet"]["description"]
+        self.thumbnail_default   = options["snippet"]["thumbnails"]["default"]["url"]
+        self.thumbnail_medium   = options["snippet"]["thumbnails"]["medium"]["url"]
+        self.thumbnail_high   = options["snippet"]["thumbnails"]["high"]["url"]
+        self.thumbnail_standard   = options["snippet"]["thumbnails"]["standard"]["url"]
+        self.thumbnail_maxres   = options["snippet"]["thumbnails"]["maxres"]["url"]
+      end
+    end
+
   end
 end
--- lib/you_tube/resources/videos.rb
+++ lib/you_tube/resources/videos.rb
@@ -1,8 +1,9 @@
 module YouTube
   class VideosResource < Resource
     
-    def list
-      response = get_request "videos", params: {chart: "mostPopular"}
+    def list(id:)
+      parts = ["contentDetails", "fileDetails", "id", "localizations", "processingDetails", "recordingDetails", "snippet", "statistics", "status", "suggestions", "topicDetails"]
+      response = get_request "videos", params: {id: id, part: parts.join(",")}
       # response = post_request("games", body: "fields id,name,status,url,created_at,updated_at;")
       Collection.from_response(response, type: Video)
     end
