4b9d5fa845d1c951e1d771b22f7bc6dfd09c2fdf
Dean Perry <dean@deanpcmad.com>
2021-12-30 15:37:07 +0000

    create and delete playlists

--- lib/you_tube/resources/playlists.rb
+++ lib/you_tube/resources/playlists.rb
@@ -7,5 +7,26 @@ module YouTube
       Collection.from_response(response, type: Playlist)
     end
 
+    def create(title:, **attributes)
+      attrs = {}
+      attrs[:snippet] = {}
+      attrs[:status] = {}
+
+      attrs[:snippet][:title] = title
+
+      attrs[:snippet][:description]     = attributes[:description]
+      attrs[:snippet][:defaultLanguage] = attributes[:default_language]
+
+      attrs[:status][:privacyStatus]    = attributes[:privacy_status]
+
+      response = post_request("playlists?part=id,snippet,status", body: attrs)
+      
+      Playlist.new(response.body) if response.success?
+    end
+
+    def delete(id:)
+      delete_request("playlists?id=#{id}")
+    end
+
   end
 end
