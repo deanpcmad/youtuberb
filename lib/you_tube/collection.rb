507c36895d3d37cd96ac3338fe22c9153813d70a
Dean Perry <dean@deanpcmad.com>
2022-01-02 12:56:32 +0000

    next and prev page tokens

--- lib/you_tube/collection.rb
+++ lib/you_tube/collection.rb
@@ -1,6 +1,6 @@
 module YouTube
   class Collection
-    attr_reader :data, :total
+    attr_reader :data, :total, :next_page_token, :prev_page_token
 
     def self.from_response(response, type:)
       body = response.body
@@ -8,13 +8,17 @@ module YouTube
       new(
         data: body["items"].map { |attrs| type.new(attrs) },
         total: body["pageInfo"]["totalResults"],
+        next_page_token: body["nextPageToken"],
+        prev_page_token: body["prevPageToken"],
         # cursor: body.dig("pagination", "cursor")
       )
     end
 
-    def initialize(data:, total:)
+    def initialize(data:, total:, next_page_token:, prev_page_token:)
       @data = data
       @total = total
+      @next_page_token = next_page_token
+      @prev_page_token = prev_page_token
     end
   end
 end
