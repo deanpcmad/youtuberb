cddbf9f0228f20d2f76332c3c6ef958808980f5b
Dean Perry <dean@deanpcmad.com>
2022-02-02 17:26:32 +0000

    liked videos and update so retrieve gets extra parts

--- lib/you_tube/resources/videos.rb
+++ lib/you_tube/resources/videos.rb
@@ -1,15 +1,26 @@
 module YouTube
   class VideosResource < Resource
 
-    PARTS = "id,contentDetails,fileDetails,processingDetails,snippet,statistics,status,suggestions"
+    PARTS = "id,contentDetails,snippet,statistics,status"
+    AUTH_PARTS = "id,contentDetails,fileDetails,processingDetails,snippet,statistics,status,suggestions"
     
+    # Retrieves Videos by ID. Can be comma separated to retrieve multiple.
     def list(id:)
       response = get_request "videos", params: {id: id, part: PARTS}
       Collection.from_response(response, type: Video)
     end
 
+    # Retrieves liked Videos for the currently authenticated user
+    def liked
+      response = get_request "videos", params: {myRating: "like", part: PARTS}
+      Collection.from_response(response, type: Video)
+    end
+
+    # Retrieves a Video by the ID. This retrieves extra information so will only work
+    # on videos for an authenticated user
     def retrieve(id:)
-      response = get_request "videos", params: {id: id, part: PARTS}
+      response = get_request "videos", params: {id: id, part: AUTH_PARTS}
+      return nil if response.body["pageInfo"]["totalResults"] == 0
       Video.new(response.body["items"][0])
     end
 
