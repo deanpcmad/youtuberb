e11fa34858e15c15b89f34a0b68cb8d3936c9333
Dean Perry <deanperry@fastmail.com>
2023-11-01 12:36:08 +0000

    also send `contentDetails`

--- lib/you_tube/resources/channels.rb
+++ lib/you_tube/resources/channels.rb
@@ -1,7 +1,7 @@
 module YouTube
   class ChannelsResource < Resource
 
-    PARTS = "id,snippet,status,statistics"
+    PARTS = "id,snippet,status,statistics,contentDetails"
 
     # Retrieve the channel of the currently authenticated user
     def mine
@@ -9,12 +9,13 @@ module YouTube
       return nil if response.body["pageInfo"]["totalResults"] == 0
       Channel.new(response.body["items"][0])
     end
-    
+
     # Retrieve a Channel by its ID or Username
     def retrieve(id: nil, username: nil)
       attrs = {}
       attrs[:id] = id if id
       attrs[:forUsername] = username if username
+      raise ArgumentError, "Must provide either an ID or Username" if attrs.empty?
 
       response = get_request "channels", params: attrs.merge({part: PARTS})
       return nil if response.body["pageInfo"]["totalResults"] == 0
