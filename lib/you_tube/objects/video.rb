d0734a213706072c5adcaf8bab9a85670efe27fb
Dean Perry <deanperry@fastmail.com>
2023-12-21 11:02:23 +0000

    age_restricted

--- lib/you_tube/objects/video.rb
+++ lib/you_tube/objects/video.rb
@@ -23,6 +23,7 @@ module YouTube
       # Is a Video is blocked in any countries?
       if options["contentDetails"]
         self.blocked = !options["contentDetails"]["regionRestriction"].nil?
+        self.age_restricted = !options["contentDetails"]["contentRating"].nil? && options["contentDetails"]["contentRating"]["ytRating"] == "ytAgeRestricted"
       end
 
       if options["liveStreamingDetails"]
