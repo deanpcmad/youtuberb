1958eed09246a78682417de636441d6c7d1196b2
Dean Perry <deanperry@fastmail.com>
2025-12-03 11:38:08 +0000

    underscore

--- lib/you_tube/object.rb
+++ lib/you_tube/object.rb
@@ -8,12 +8,21 @@ module YouTube
 
     def to_ostruct(obj)
       if obj.is_a?(Hash)
-        OpenStruct.new(obj.map { |key, val| [key, to_ostruct(val)] }.to_h)
+        OpenStruct.new(obj.map { |key, val| [underscore(key), to_ostruct(val)] }.to_h)
       elsif obj.is_a?(Array)
         obj.map { |o| to_ostruct(o) }
       else # Assumed to be a primitive value
         obj
       end
+    end
+
+    private
+
+    def underscore(str)
+      str.to_s
+         .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+         .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+         .downcase
     end
   end
 end
